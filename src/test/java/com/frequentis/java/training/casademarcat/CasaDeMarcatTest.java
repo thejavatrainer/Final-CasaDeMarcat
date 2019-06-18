c9b598e6a81d8518af7bc748131cb4dee116950c
rszabi <ruginaszabolcs@gmail.com>
2019-06-18 12:04:25 +0300

    Initial

---
+++ src/test/java/com/frequentis/java/training/casademarcat/CasaDeMarcatTest.java
@@ -0,0 +1,17 @@
+package com.frequentis.java.training.casademarcat;
+
+public class CasaDeMarcatTest {
+    private Cashier casa;
+
+    @Before
+    public void setup() {
+        casa = new Cashier();
+    }
+
+
+
+    @After
+    public void teardown() {
+
+    }
+}
